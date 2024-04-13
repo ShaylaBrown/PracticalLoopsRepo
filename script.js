27d3806e63414a448623e8937a22b524b6cc34ab
Shayla Brown <shaylabrown227@gmail.com>
2024-04-13 13:17:56 -0500

    Looped numbers 1-100

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Practial Loops")
+console.log("Loop through all numbers from 1-100")
+for (let a = 1; a <= 100; a++) {
+	console.log(a)
+}
+console.log("Log Fizz if number divivisible by 3")
