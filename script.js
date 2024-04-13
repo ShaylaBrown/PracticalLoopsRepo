55b181e475100151f9ff7ff61fcb714a4214f633
Shayla Brown <shaylabrown227@gmail.com>
2024-04-13 15:19:43 -0500

    completed part 2

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ for (var i = 1; i < 101; i++) {
     else if (i % 5 == 0) console.log("Buzz");
     else console.log(i);
 }
+console.log("==========Part 2: Prime Time===========")
+for (let n = 2; n <= 3; n++) {
+    let isPrime = true
+
+    for (let j = 2; j < n; j++) {
+        if (n % j === 0) {
+            // if this is true, it is NOT a prime number
+            isPrime = false
+            break
+        }
+    }
+
+    if (isPrime === true) {
+        console.log(`${n}` + isPrime)
+    }
+
+
+}
+
